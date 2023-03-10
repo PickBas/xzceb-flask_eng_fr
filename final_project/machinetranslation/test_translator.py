aaaf096e5e8ff4ed225d0153361211cefb126e6b
Kirill Saied <sayed.kirill@gmail.com>
2023-03-10 13:09:42 +0300

    Added assertNotEqual

--- final_project/machinetranslation/test_translator.py
+++ final_project/machinetranslation/test_translator.py
@@ -9,11 +9,13 @@ class TestMachineTranslation(unittest.TestCase):
         self.assertEqual(englishToFrench("Hello"), "Bonjour")
         self.assertEqual(englishToFrench("How are you?"), "Comment es-tu?")
         self.assertEqual(englishToFrench("Where are you?"), "Où es-tu?")
+        self.assertNotEqual(englishToFrench("How are you?"), "Où es-tu?")
     
     def test_french_to_english(self):
         self.assertEqual(frenchToEnglish("Bonjour"), "Hello")
         self.assertEqual(frenchToEnglish("Comment es-tu?"), "How are you?")
         self.assertEqual(frenchToEnglish("Où es-tu?"), "Where are you?")
+        self.assertNotEqual(frenchToEnglish("Où es-tu?"), "How are you?")
 
 if __name__ == '__main__':
     unittest.main()
